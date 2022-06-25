25615466a3dd05ce03c9f316b76379be79a35fa8
Simar <smudhar_be21@thapar.edu>
2022-06-25 10:50:21 +0530

    Initial commit

---
+++ src/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Categories = ({filterItems}) => {
+  return <div className='btn-container'>
+    <button className='filter-btn' onClick={()=>filterItems('all')}>All</button>
+    <button className='filter-btn' onClick={()=>filterItems('breakfast')}>Breakfast</button>
+    <button className='filter-btn' onClick={()=>filterItems('lunch')}>lunch</button>
+    <button className='filter-btn' onClick={()=>filterItems('shakes')}>shakes</button>
+   </div>
+};
+
+export default Categories;
